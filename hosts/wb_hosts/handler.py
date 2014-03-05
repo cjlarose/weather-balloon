8ecb061efbfcedb88df3e2af2744c3ac0ab9670b
Chris La Rose <cjlarose@iplantcollaborative.org>
2014-03-05 13:31:18 -0700

    Correctly setting hosts dynamically

--- hosts/wb_hosts/handler.py
+++ hosts/wb_hosts/handler.py
@@ -1,16 +1,41 @@
+from pynag.Model import Host
+from pynag.Control.Command import send_command
+
 from wb_services.hosts import Hosts
 
-class Handler(Hosts.Iface):
-    def __init__(self):
-        pass
+ATMO_HOSTGROUP_NAME = 'atmo-vms'
+ATMO_HOST_TEMPLATE = 'atmo_vm'
+ATMO_HOST_FILE = '/etc/nagios/atmo-hosts.cfg'
 
-    def set_hosts(hosts):
+class Handler(Hosts.Iface):
+
+    @staticmethod
+    def to_pynag_host(host):
+        new_host = Host()
+        new_host.host_name = host.id
+        new_host.alias = new_host.display_name = host.display_name
+        new_host.address = host.address
+        new_host.hostgroups = ATMO_HOSTGROUP_NAME
+        new_host.use = ATMO_HOST_TEMPLATE
+        new_host.set_filename(ATMO_HOST_FILE)
+        return new_host
+
+    def set_hosts(self, hosts):
         """
         Write Nagios config file; restart daemon
         """
-        pass
+        # Remove All monitored hosts
+        for host in Host.objects.filter(hostgroups=ATMO_HOSTGROUP_NAME):
+            host.delete()
 
-    def get_metrics(host_name, key):
+        # Add all passed-in hosts
+        for host in [Handler.to_pynag_host(h) for h in hosts]:
+            host.save()
+
+        # Restart the nagios daemon
+        send_command("RESTART_PROGRAM")
+
+    def get_metrics(self, host_name, key):
         """
         Query graphite for metrics
         """
