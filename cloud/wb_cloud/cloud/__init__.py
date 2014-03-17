48d9b24c7105def07ee42e51e9968dffeaf0e4b7
Chris LaRose <cjlarose@iplantcollaborative.org>
2014-03-17 10:41:22 -0700

    Disable all clouds but OpenStack for now

--- cloud/wb_cloud/cloud/__init__.py
+++ cloud/wb_cloud/cloud/__init__.py
@@ -1,3 +1,3 @@
-from ec2_cloud import EucaCloud, AWSCloud
-from openstack import OpenStackCloud
+#from ec2_cloud import EucaCloud, AWSCloud
+#from openstack import OpenStackCloud
 from openstack_rtwo import OpenStackRtwoCloud
