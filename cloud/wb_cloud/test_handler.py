633aba5230dfaa64acb9f224f42c282d9ff35aa1
Chris LaRose <cjlarose@iplantcollaborative.org>
2014-04-01 14:00:00 -0700

    Remove test handler file

--- cloud/wb_cloud/test_handler.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-from wb_cloud.settings import config
-from wb_cloud.handler import Handler
-
-engine = create_engine(config['db_url'])
-Session = sessionmaker(bind=engine)
-handler = Handler(Session())
