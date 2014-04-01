8852746fe3bcf655b05c83e402a1f38ed8c8ce07
Chris LaRose <cjlarose@iplantcollaborative.org>
2014-04-01 09:39:04 -0700

    Update thriftfile for cloud service

--- thriftfiles/cloud.thrift
+++ thriftfiles/cloud.thrift
@@ -5,8 +5,10 @@ struct UserStats {
     2: i32 total_uptime_seconds; // not adjusted by number of cpus
     3: i32 instance_count;
     4: bool is_staff;
+    5: string username;
 }
 
 service Cloud {
-    list<UserStats> get_leaderboard();
+    list<UserStats> get_leaderboard(1: i32 start_time, 2: i32 end_time);
+    list<UserStats> get_user_stats(1: i32 start_time, 2: i32 end_time, 3: string username);
 }
